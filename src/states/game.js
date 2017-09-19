2b1df9eb63b1ca7d1508be3ca139416ee5950ebe
JS Rolón <js.rolon554@uniandes.edu.co>
2017-09-19 19:51:39 -0400

    learning phaser

--- src/states/game.js
+++ src/states/game.js
@@ -1,50 +1,41 @@
-import Crosshairs from '../prefabs/crosshairs';
-import Target from '../prefabs/target';
-
 class Game extends Phaser.State {
 
-  keyDown = false;
-  wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
-
-  constructor() {
-    super();
-  }
-  
-  create() {
-    //setup UI
-    this.countdownText = this.add.text(this.game.world.centerX, 0, '', {
-      font: '40px Arial', fill: '#ffffff', align: 'center'
-    });
-    this.countdownText.anchor.set(0.5,0);
-
-    //set up click listeners
-    this.game.input.onDown.add(this.shoot, this);
-
-    //setup a timer to end the game
-    this.endGameTimer = this.game.time.create();
-    this.endGameTimer.add(Phaser.Timer.SECOND * 10, this.endGame,this);
-    this.endGameTimer.start();
-  }
-
-  update() {
-    if(this.wKey.isDown) {
-      if(!this.keyDown) {
-          this.game.global.score++;
-      }
-      this.keyDown = true;
-    } else {
-      this.keyDown = false;
+    constructor() {
+        super();
     }
-  }
 
-  endGame() {
-    if(this.game.global.score > 0) {
-      this.game.state.start('win');
-    } else {
-      this.game.state.start('gameover');
+    create() {
+        //setup UI
+        this.countdownText = this.add.text(this.game.world.centerX, 0, 'Pres W as many times as possible', {
+            font: '40px Arial', fill: '#ffffff', align: 'center'
+        });
+        this.countdownText.anchor.set(0.5, 0);
+
+        //set up click listeners
+        this.game.input.keyboard.addCallbacks(this, this.keyTap);
+
+        //setup a timer to end the game
+        this.endGameTimer = this.game.time.create();
+        this.endGameTimer.add(Phaser.Timer.SECOND * 10, this.endGame, this);
+        this.endGameTimer.start();
     }
-  }
 
+    keyTap() {
+        if(this.game.input.keyboard.lastKey === Phaser.Keyboard.W) {
+            this.game.global.score++;
+        }
+        console.log(this.game.global.score);
+    }
+
+    endGame() {
+        this.game.input.keyboard.stop();
+
+        if (this.game.global.score > 10) {
+            this.game.state.start('win');
+        } else {
+            this.game.state.start('gameover');
+        }
+    }
 }
 
 export default Game;
